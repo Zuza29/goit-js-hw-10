635b717a2a29d5adf3cf6ba6388c57f90f191212
Zuzanna Kaźmierczak <zusapphire@gmail.com>
2022-11-22 15:45:07 +0100

    Work in progress

--- src/Monika.js
+++ src/Monika.js
@@ -1,44 +1,55 @@
+
+
 import debounce from 'lodash.debounce';
 import { fetchCountries } from './js/fetchCountries.js';
 import './css/styles.css';
 import Notiflix from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
+
 const input = document.getElementById('search-box');
+
 const countriesList = document.getElementsByClassName('country-list');
 
 const createCountry = country => {
     console.log(countriesList[0]);
     const li = document.createElement('li');
-    li.innerHTML = `<div class='container'><img src=${country.flags.svg} /><span class="country-name">${country.name.official}</span></div>`
+    li.innerHTML = `<img src=${country.flags.svg} /><span>${country.name.official}</span>`
     countriesList[0].appendChild(li);
-};
-
-const removeCountry = () => {
-    let element = document.querySelector('ul');
-    while (element.firstChild) {
-        element.removeChild(element.firstChild);
-    };
-};
+}
 
 const handleInput = async (event) => {
     const response = await fetchCountries(event.target.value && event.target.value.trim());
+    // sprawdzamy status, 200 to znaczy, ze jest spoko, bez błędów
     if (response.status === 200) {
         const countries = await response.json();
         if (countries && countries.length >= 10) {
             Notiflix.Notify.info('Too many matches found. Please enter a more specific name');
         } else if (countries) {
-            removeCountry();
+            // tworzymy element
             countries.forEach(country => createCountry(country));
         }
-        else if (response.status === 404) {
-            Notiflix.Notify.failure('Oops, there is no country with that name');
-        }
-    };
-};
+    } else if (response .status === 404) {
+        Notiflix.Notify.failure('Oops, there is no country with that name');
+    }
+
+}
 
 input.addEventListener('input', debounce(handleInput, DEBOUNCE_DELAY));
 
+
+// fetchCountries.js
+
+const BASE_URL = 'https://restcountries.com/v3.1'
+
+function fetchCountries(name) {
+  // podstawowy url + właściwości jakie chcemy wyciagnąć
+  return fetch(`${BASE_URL}/name/${name}?fields=name,capital,population,flags,languages`)
+};
+
+export {
+  fetchCountries
+};
 
 
 
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,15 +1,13 @@
-const fetchCountries = (name) => {
-    fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages,region`)
-        .then(response => {
-            if (!response.ok) {
-                return Promise.reject(new Error('Oops, there is no country with that name'));
-            }
-            const data = response.json();
-            return data;
-        })
-        .catch(error => {
-            console.log(`Your request could not be fulfilled. Error code: ${error}`);
-        })
-}
+const BASE_URL = 'https://restcountries.com/v3.1'
 
-export { fetchCountries };
+function fetchCountries(name) {
+    // podstawowy url + właściwości jakie chcemy wyciagnąć
+    return fetch(`${BASE_URL}/name/${name}?fields=name,capital,population,flags,languages`)
+};
+
+export {
+    fetchCountries
+};
+// 1. Czy można to zrobić bez użycia async/await?
+// 2. Czemu funkcja fetchCountries jest taka krótka? Bez sensu ją robić od razu bardzo
+// długą i ze skomplikowaną funkcjonalnością?
